2d9037f84b30cf23df44be4e478d827874275813
Shaun Miller <shaunmiller1024@gmail.com>
2020-03-29 19:21:42 -0400

    initial db design working

--- psimple/app1/app1.py
+++ psimple/app1/app1.py
@@ -6,6 +6,7 @@ import time
 import sys
 import stomp                # For Apache MQ messaging
 import re
+import sqlite3
 
 class MyListener(stomp.ConnectionListener):
     def on_error(self, headers, message):
@@ -20,12 +21,35 @@ class MyListener(stomp.ConnectionListener):
         if match_object:
             message_queue.append(match_object)
 
+def create_initial_tables(db_connection):
+    db_cursor = db_connection.cursor()
+
+    db_cursor.execute('create table ibcp_config (config_item TEXT primary key, key TEXT, value TEXT)')
+    db_cursor.execute('create table ibcp_robots (serial_number TEXT PRIMARY KEY)')
+
+    db_connection.commit()
+    db_connection.close()
+
+# Begin main code:
 message_queue = []
 
 player_one_serial = "00508a44"
 player_two_serial = "0060689b"
 mq_server = "192.168.1.153"
 mq_port = "61613"
+
+# DB:
+current_directory = os.getcwd()
+
+db_connection = sqlite3.connect(current_directory + "/ibcp.db")
+db_cursor = db_connection.cursor()
+
+if (os.stat(current_directory + "/ibcp.db").st_size == 0):
+    print ("Need to create tables")
+    create_initial_tables(db_connection)
+
+else:
+    print ("ibcp.db exists, no need to create")
 
 # Create connection to MQ server.
 try:
@@ -49,15 +73,37 @@ if player_two_serial:
 
 sg.theme('Dark Blue 3')
 
+# ------ Menu Definition ------ #
+menu_def = [['&File', ['E&xit']],
+            ['&Edit', ['Preferences'] ],
+            ['&Help', '&About...'], ]
+
+layout_preferences = [
+    [sg.Text('MQ Server:', size=(9, 1)), sg.InputText(default_text='', key='-MQSERVER-', size=(16, 1))],
+    [sg.Text('MQ port:', size=(10, 1)), sg.InputText(default_text='61613', key='-MQPORT-', size=(7, 1))],
+    [sg.Button('Set'), sg.Button('Close')]
+]
+
 layout =    [
+            [sg.Menu(menu_def, tearoff=True)],
             [sg.Text('Welcome to IBCP huamn interface!', size=(50, 1))],
-            [sg.Multiline(default_text='', size=(200,20), autoscroll=True, do_not_clear=True, enter_submits=True, key='-OUTPUT-')],
-            [sg.Text('Enter some input', size=(15,1))],
-            [sg.Text('Guess a number', size=(15,1)), sg.InputText('', key='-INPUT-')],
+            [sg.Multiline(default_text='', size=(199, 40), autoscroll=True, do_not_clear=True, enter_submits=True, key='-OUTPUT-')],
+            [
+                sg.Multiline(default_text='Instructions go here', size=(64, 10), autoscroll=True, do_not_clear=True, enter_submits=True, key='-INSTRUCTIONS-'),
+                sg.Listbox(default_values='', values='', size=(64, 10), key='-ROBOTS-'),
+                sg.Listbox(default_values='', values='', size=(64, 10), key='-APPS-')
+            ],
+            [
+                sg.Text('Enter some input', size=(66, 1)),
+            ],
+            [
+                sg.Text('Guess a number', size=(15, 1)), sg.InputText('', key='-INPUT-', size=(10, 1)),
+
+            ],
             [sg.Submit(), sg.Cancel()]
             ]
 
-window = sg.Window('What goes here?', layout)
+window = sg.Window('IBCP version 0.2', layout, resizable=True)
 
 event, values = window.read()
 
@@ -66,28 +112,73 @@ output_list = []
 command_ran = False
 
 while True:
-    '''
-    event, values = window.read(timeout=100)
+    if event == "Preferences":
+        preferences_window = sg.Window('Preferences', layout_preferences, resizable=True, location=[800,400])
 
-    print ("values is: " + str(values))
+        preferences_window.Finalize()
 
-    print ("output of multiline: " + str(window['-OUTPUT-'].update(values['-INPUT-'])))
+        # I think here we do a select to get the current mq_server and mq_port values and populate
+        # the fields with the data.
+        db_cursor.execute("select * from ibcp_config where config_item='mq_config'")
 
-    if event is None or event == 'Exit' or event == 'Cancel':
-        break
+        rows = db_cursor.fetchall()
 
-    '''
+        for row in rows:
+            if row[0] == "mq_config":
+                preferences_window['-MQSERVER-'].update(row[1])
+                preferences_window['-MQPORT-'].update(row[2])
 
-    print ("I am doing a read here.")
+
+        event_preferences, event_values = preferences_window.read()
+
+        while True:
+            print ("What is event_preferences before read: " + event_preferences)
+            print ("What is event_values before read: " + str(event_values))
+
+            if event_preferences == 'Set':
+                mq_server = event_values['-MQSERVER-']
+                mq_port = event_values['-MQPORT-']
+
+                # First we have to see if the record exists:
+                db_connection = sqlite3.connect(current_directory + "/ibcp.db")
+                db_cursor = db_connection.cursor()
+                db_cursor.execute("select count(*) from ibcp_config")
+
+                rows = db_cursor.fetchall()
+
+                row_count = rows[0]
+
+                print ("what is row_count: ***" + str(row_count[0]) + "***")
+
+                if row_count[0] == 0:
+                    #insert
+                    print ("insert statement is: " + "insert into ibcp_config values ('mq_config', '" + mq_server + "', '" + mq_port + "')")
+                    db_cursor.execute("insert into ibcp_config values ('mq_config', '" + mq_server + "', '" + mq_port + "')")
+                else:
+                    print ("update code here")
+                    db_cursor.execute("update ibcp_config set key='" + mq_server + "',value='" + mq_port + "' where config_item='mq_config'")
+
+
+                db_connection.commit()
+                db_connection.close()
+
+            if event_preferences is None or event_preferences == 'Exit' or event_preferences == 'Cancel' or event_preferences == 'Close':
+                preferences_window.close()
+                break
+
+            event_preferences, event_values = preferences_window.read(timeout=1000)
+
+    print ("What is event before read: " + event)
+    print ("What is values before read: " + str(values))
 
     if event is None or event == 'Exit' or event == 'Cancel':
         break
 
     event, values = window.read(timeout=1000)
-    print (event, values)
 
     if not command_ran:
-        p = subprocess.Popen("/mnt/backups/anki/IBCP/applications/number_guesser/number_guesser.py -c /mnt/backups/anki/IBCP/ibcp.cfg --p1 vector:00508a44 --p2 vector:0060689b", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        print ("number guesser being called here...")
+        #p = subprocess.Popen("/mnt/backups/anki/IBCP/applications/number_guesser/number_guesser.py -c /mnt/backups/anki/IBCP/ibcp.cfg --p1 vector:00508a44 --p2 vector:0060689b", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         command_ran = True
 
     if command_ran:
@@ -122,38 +213,6 @@ while True:
         # Remove the MQ message from the message_queue[] list now that we have processed it.
         message_queue.remove(message)
 
-
-
-    # Run command here:
-    #output = subprocess.check_output(['/usr/bin/tail', '-n', '10', '/var/log/syslog'], universal_newlines=True)
-    #process = subprocess.Popen(['/usr/bin/vmstat', '1', '5'], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
-    #pipe = Popen(['/usr/bin/vmstat', '1', '5'], stdout=PIPE, universal_newlines=True, shell=True)
-
-    #if not command_ran:
-        #pipe = Popen([ '/mnt/backups/anki/IBCP/applications/number_guesser/number_guesser.py', '-c', '/mnt/backups/anki/IBCP/ibcp.cfg', '--p1', 'vector:0060100c', '--p2', 'vector:0060689b' ], stdout=PIPE, universal_newlines=True, shell=True)
-        #pipe = Popen("/mnt/backups/anki/IBCP/applications/number_guesser/number_guesser.py -c /mnt/backups/anki/IBCP/ibcp.cfg --p1 vector:0060100c --p2 vector:0060689b", stdout=PIPE, universal_newlines=True, shell=False)
-        #pipe = Popen("/mnt/backups/anki/IBCP/psimple/app1/wrapper.sh", stdout=PIPE, universal_newlines=True, shell=True)
-
-
-
-    #window['-OUTPUT-'].update(payload, append=True)
-    #window.Refresh()
-
-    #output_splitlines = output.stdout.splitlines()
-
-    #for record in output_splitlines:
-    #    window['-OUTPUT-'].update(record + "\n", append=True)
-
-
-    #output_splitlines = output.split("\n")
-
-    #for record in output_splitlines:
-    #    window['-OUTPUT-'].update(record)
-
-
-
-    #command_ran = True
     time.sleep(0.1)
 
 window.close()
